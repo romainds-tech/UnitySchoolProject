22585d1370177b11d0636dfc165815c3d9b4b4e9
romain.da-silva@etu.univ-smb.fr <romain.da-silva@etu.univ-smb.fr>
2022-09-27 11:08:03 +0200

    init

---
+++ Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.localPosition = new Vector3(this.transform.localPosition.x + 0.1f, this.transform.localPosition.y , this.transform.localPosition.z);
+        
+        // this.transform.localPosition = new Vector3(0.1f, 0, 0);
+        
+        // this.transform.localPosition = Vector3.right * 0.1f;
+        
+        this.transform.Rotate(Vector3.up * 10f);
+    }
+}
